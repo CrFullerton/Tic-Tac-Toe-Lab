653521275cbcd8ef8893b9ed3519653b7a24b678
CrystalFullerto <68161845+CrFullerton@users.noreply.github.com>
2022-01-10 03:43:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Lab - Tic-Tac-Toe 
+Implement the Tic Tac Toe game using a single list
+Create this game again using lists and indexes. 
+
+** Rules are below: ** <br>
+Allow users to keep playing (max 9 times). <br>
+Print the diagram before play begins:
+
+
+     1 | 2 | 3  
+    -----------
+     4 | 5 | 6  
+    -----------
+     7 | 8 | 9
+
+<li>Use variables to track whose turn it is. 
+<li>Greet the players as “X's” or “O's”.
+<li>User picks a location on the board by entering a number.
+<li>Depending on the location that the user chose, update the corresponding board position.
+<li> If the player attempts to play a position that has already been taken the player should be prompted to choose a different location on the board.  
+<li>Print the updated board out. 
+<li>There are eight possible ways to win a Tic-Tac-Toe game. After each turn in the game, check to see if the most recent player has won the game.
+<li>Print appropriate messages if the X's player wins, or if the O's player wins.
+<li>If no one has won the game after 9 moves, declare the game to be a draw.
---
+++ main.py
@@ -0,0 +1,91 @@
+import sys
+#Tic-tac-Toe starter code
+
+def reset_board():
+    board = []
+    for i in range(1,10):
+        board.append(str(i))
+    return(list(board))
+
+
+#Takes in current game board and prints team positions 
+def print_board(board):
+    #since the code above can be any length - we need to set the print to accomodate it
+    breakrow = 3
+    board_print = "\n "
+    for b in range(0,len(board)):
+        # for every time we hit breakrow, no vertical, and print the dash row
+        #but no dash row at the end 
+        if (b+1) % breakrow != 0:
+            board_print += board[b]+ " | "
+        elif (b+1) < len(board)-1:
+            board_print += board[b]+"\n ---------\n "
+        else:
+            board_print += board[b]
+    board_print += "\n"
+    print(board_print)
+
+
+
+#TODO:
+#Create a function or multiple functions to check for a winner
+def check_for_winner(board):
+    pass
+    """
+    check_for_winner(board) determines if their is a draw or a winner 
+    :param board: list with current game state and available positions
+    :return: No return; prints game 
+"""
+    #TO-DO:
+    #Check for diagonal winner
+    ##Determin what else decides a winner
+   
+            
+def keep_playing():
+    """
+    Asks user if they would like to continue game play
+    :param None
+    :ouput: resets game if user decides to play again and exits program otherwise. 
+    """
+    x = input("Would you like to play again? y or n? ").lower()
+    if (x == "n"):
+        print("Thank you for playing our game!")
+        sys.exit("Thank you for playing our game!")
+    else:
+        current_board = []
+        player_one = input('Would you like to be "X\'s" or "O\'s: '  ).upper()
+        reset_board()
+        
+        
+
+#Game Loop
+game_over = False
+winner = -1
+player_one = input('Would you like to be "X\'s" or "O\'s: '  ).upper()
+current_board = reset_board()
+
+while (not game_over):
+    #TO-DO:
+    #complete game loop below
+    #make a plan using psuedo code. 
+    print_board(current_board)
+
+
+    #keep_playing is called below in the starter code to allow us to see the game loop. Decide as a class if it is needed. 
+    keep_playing()
+    if (winner != -1):
+        game_over = True
+        keep_playing()
+    
+
+
+        
+
+
+
+
+
+
+
+
+
